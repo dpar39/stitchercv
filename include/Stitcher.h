5313a82551dcd89f89dd93777a8857de831828de
Darien Pardinas Diaz <dpar39@gmail.com>
2019-03-13 21:33:04 -0400

    Some more work

--- include/Stitcher.h
+++ include/Stitcher.h
@@ -2,16 +2,29 @@
 
 #include <opencv2/core/core.hpp>
 
+namespace cv::xfeatures2d
+{
+class SIFT;
+}
+
 class Stitcher
 {
 
 public:
+    Stitcher();
+
     void processVideo(const std::string & inputVideoFile);
 
 private:
     void splitInputImage(const cv::Mat & frame);
 
-    void detectAndDescribe(const cv::Mat & framePart);
+    cv::Mat computeHomography(const cv::Mat & leftImage, const cv::Mat & rightImage);
 
     cv::Mat m_images[4];
+
+    cv::Mat m_leftImageMask;
+
+    cv::Mat m_rightImageMask;
+
+    cv::Ptr<cv::xfeatures2d::SIFT> m_sift;
 };
--- src/Stitcher.cpp
+++ src/Stitcher.cpp
@@ -2,13 +2,23 @@
 
 #include "Stitcher.h"
 
+#include <cmath>
+#include <filesystem>
+
+#include <opencv2/calib3d/calib3d.hpp>
 #include <opencv2/features2d/features2d.hpp>
+#include <opencv2/imgcodecs/imgcodecs.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/videoio/videoio.hpp>
 
 #include <opencv2/xfeatures2d/nonfree.hpp>
 
 using namespace cv;
+
+Stitcher::Stitcher()
+{
+    m_sift = cv::xfeatures2d::SIFT::create();
+}
 
 void Stitcher::processVideo(const std::string & inputVideoFile)
 {
@@ -30,6 +40,28 @@ void Stitcher::processVideo(const std::string & inputVideoFile)
             break;
 
         splitInputImage(frame);
+
+        cv::Mat H;
+        if (std::filesystem::exists("H.xml"))
+        {
+            FileStorage fs;
+            fs.open("H.xml", FileStorage::READ);
+            fs["H"] >> H;
+        }
+        else
+        {
+            H = computeHomography(m_images[0], m_images[1]);
+            cv::FileStorage file("H.xml", cv::FileStorage::WRITE);
+            file << "H" << H;
+        }
+
+        auto h = H.inv();
+
+        cv::Mat result;
+
+        cv::warpPerspective(m_images[1], result, h, Size(1080 * 2, 1920));
+
+        m_images[0].copyTo(result(Rect(0, 0, 1080, 1920)));
     }
 
     cap.release();
@@ -46,19 +78,65 @@ void Stitcher::splitInputImage(const cv::Mat & frame)
         roi.x = i * iWidth;
         m_images[i] = frame(roi);
 
-        detectAndDescribe(m_images[i]);
+        imwrite("D:\\GITHUB\\stitchcv\\thirdparty\\SimplePanoStitcher\\sample\\00" + std::to_string(i) + ".png",
+                m_images[i]);
     }
 }
 
-void Stitcher::detectAndDescribe(const cv::Mat & framePart)
+cv::Mat Stitcher::computeHomography(const cv::Mat & leftImage, const cv::Mat & rightImage)
 {
     cv::Mat grayPart;
-    cv::Mat descriptors;
-    cv::cvtColor(framePart, grayPart, cv::COLOR_BGR2GRAY);
 
-    const auto sift = cv::xfeatures2d::SIFT::create();
+    const auto horizontalOverlap = static_cast<int>(ceil((1080.f * 4 - 3648) / 3.f)); // ~20% overlap
 
-    std::vector<KeyPoint> keyPoints;
+    const auto height = leftImage.size[0];
+    const auto width = leftImage.size[1];
 
-    sift->detectAndCompute(framePart, cv::Mat(), keyPoints, descriptors);
+    if (m_leftImageMask.empty())
+    {
+        m_leftImageMask = cv::Mat::zeros(leftImage.size(), CV_8U);
+        m_leftImageMask(cv::Rect(width - horizontalOverlap - 1, 0, horizontalOverlap, height)) = 1;
+    }
+
+    if (m_rightImageMask.empty())
+    {
+        m_rightImageMask = cv::Mat::zeros(leftImage.size(), CV_8U);
+        m_rightImageMask(cv::Rect(0, 0, horizontalOverlap, height)) = 1;
+    }
+
+    cv::cvtColor(leftImage, grayPart, cv::COLOR_BGR2GRAY);
+
+    std::vector<KeyPoint> keyPointsLeft, keyPointsRight;
+    cv::Mat descriptorsLeft, descriptorsRight;
+
+    m_sift->detectAndCompute(leftImage, m_leftImageMask, keyPointsLeft, descriptorsLeft);
+    m_sift->detectAndCompute(rightImage, m_rightImageMask, keyPointsRight, descriptorsRight);
+
+    const auto matcher = DescriptorMatcher::create(DescriptorMatcher::BRUTEFORCE);
+    std::vector<std::vector<DMatch>> knnMatches;
+    matcher->knnMatch(descriptorsLeft, descriptorsRight, knnMatches, 2);
+    //-- Filter matches using the Lowe's ratio test
+    const auto ratioThresh = 0.75f;
+    std::vector<DMatch> goodMatches;
+    for (const auto & knnMatch : knnMatches)
+    {
+        if (knnMatch[0].distance < ratioThresh * knnMatch[1].distance)
+        {
+            goodMatches.push_back(knnMatch[0]);
+        }
+    }
+
+    std::vector<Point2f> leftPoints;
+    std::vector<Point2f> rightPoints;
+    for (const auto & goodMatch : goodMatches)
+    {
+        //-- Get the key points from the good matches
+        leftPoints.push_back(keyPointsLeft[goodMatch.queryIdx].pt);
+        rightPoints.push_back(keyPointsRight[goodMatch.trainIdx].pt);
+    }
+
+    // Homgraphic transformation from Left to Right image
+    Mat H = cv::findHomography(leftPoints, rightPoints, RANSAC, 4.0);
+
+    return H;
 }
